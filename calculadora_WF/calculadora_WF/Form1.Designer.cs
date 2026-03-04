4077c88aa597815e66d2951540bbea6cada42a79
Flavio-117 <fvolacerda@hotmail.com>
2026-03-03 21:26:28 -0300

    precisa terminar ainda

--- calculadora_WF/calculadora_WF/Form1.Designer.cs
+++ calculadora_WF/calculadora_WF/Form1.Designer.cs
@@ -29,28 +29,28 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
             this.groupBox5 = new System.Windows.Forms.GroupBox();
-            this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
-            this.groupBox4.SuspendLayout();
             this.groupBox5.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -60,14 +60,96 @@
             this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(14, 5);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
             this.groupBox1.Size = new System.Drawing.Size(559, 409);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "CALCULADORA";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.button8);
+            this.groupBox5.Controls.Add(this.button7);
+            this.groupBox5.Location = new System.Drawing.Point(317, 287);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(200, 82);
+            this.groupBox5.TabIndex = 1;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "controle";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(107, 22);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(76, 43);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "sair";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(17, 22);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(76, 43);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "limpar";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Location = new System.Drawing.Point(316, 23);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(201, 258);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "equações";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(18, 199);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(166, 40);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "divisão";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(18, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "multiplicação";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "subtração";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "adição";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox2
             // 
@@ -84,97 +166,6 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "calculo";
             // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.button4);
-            this.groupBox3.Controls.Add(this.button3);
-            this.groupBox3.Controls.Add(this.button2);
-            this.groupBox3.Controls.Add(this.button1);
-            this.groupBox3.Location = new System.Drawing.Point(316, 23);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(201, 258);
-            this.groupBox3.TabIndex = 0;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "equações";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(18, 31);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(166, 40);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "adição";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(18, 87);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(166, 40);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "subtração";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(18, 143);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(166, 40);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "multiplicação";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(18, 199);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(166, 40);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "divisão";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 148);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(168, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "_______________________";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(67, 78);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(67, 122);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
-            this.textBox2.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(38, 125);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(13, 16);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "?";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(88, 168);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 16);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "resultado";
-            // 
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.button6);
@@ -184,6 +175,15 @@
             this.groupBox4.Size = new System.Drawing.Size(201, 69);
             this.groupBox4.TabIndex = 5;
             this.groupBox4.TabStop = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(110, 19);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(85, 42);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "imp/par";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
@@ -195,43 +195,48 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
-            // button6
+            // label3
             // 
-            this.button6.Location = new System.Drawing.Point(110, 19);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(85, 42);
-            this.button6.TabIndex = 1;
-            this.button6.Text = "imp/par";
-            this.button6.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "resultado";
             // 
-            // groupBox5
+            // label2
             // 
-            this.groupBox5.Controls.Add(this.button8);
-            this.groupBox5.Controls.Add(this.button7);
-            this.groupBox5.Location = new System.Drawing.Point(317, 287);
-            this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(200, 82);
-            this.groupBox5.TabIndex = 1;
-            this.groupBox5.TabStop = false;
-            this.groupBox5.Text = "controle";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "?";
             // 
-            // button7
+            // textBox2
             // 
-            this.button7.Location = new System.Drawing.Point(17, 22);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(76, 43);
-            this.button7.TabIndex = 0;
-            this.button7.Text = "limpar";
-            this.button7.UseVisualStyleBackColor = true;
+            this.textBox2.Location = new System.Drawing.Point(67, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // button8
+            // textBox1
             // 
-            this.button8.Location = new System.Drawing.Point(107, 22);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(76, 43);
-            this.button8.TabIndex = 1;
-            this.button8.Text = "sair";
-            this.button8.UseVisualStyleBackColor = true;
+            this.textBox1.Location = new System.Drawing.Point(67, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "_______________________";
             // 
             // Form1
             // 
@@ -241,15 +246,15 @@
             this.ClientSize = new System.Drawing.Size(598, 437);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "CALCULADORA";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            this.groupBox3.ResumeLayout(false);
             this.groupBox4.ResumeLayout(false);
-            this.groupBox5.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
--- calculadora_WF/calculadora_WF/Form1.cs
+++ calculadora_WF/calculadora_WF/Form1.cs
@@ -12,6 +12,10 @@ namespace calculadora_WF
 {
     public partial class Form1 : Form
     {
+        bool first;
+        bool second;
+        double num1;
+        double num2;
         public Form1()
         {
             InitializeComponent();
@@ -24,12 +28,161 @@ namespace calculadora_WF
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            label2.Text = "+";
+            //if (int.TryParse(textBox1.Text, out int num1))
+            //{
+            //    first = true;
+            //}
+            //else
+            //{
+            //    first = false;
+            //    label3.Text = "somente números, por favor";
+            //}
+            //if (int.TryParse(textBox2.Text, out int num2))
+            //{
+            //    second = true;
+            //}
+            //else
+            //{
+            //    second = false;
+            //    label3.Text = "somente números, por favor";
+            //}
+            if (first && second) 
+            {
+                label3.Text = (num1 + num2).ToString();
+            }
+            else 
+            {
+                label3.Text = "ensira dois valores válidos";
+            }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double num1))
+            {
+                first = true;
+                label3.Text = num1.ToString();
+            }
+            else
+            {
+                first = false;
+                label3.Text = "somente números, por favor";
+            }
+            ;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "-";
+            ////if (int.TryParse(textBox1.Text, out int num1))
+            ////{
+            ////    first = true;
+            ////}
+            ////else
+            ////{
+            ////    first = false;
+            ////    label3.Text = "somente números, por favor";
+            ////}
+            //if (int.TryParse(textBox2.Text, out int num2))
+            //{
+            //    second = true;
+            //}
+            //else
+            //{
+            //    second = false;
+            //    label3.Text = "somente números, por favor";
+            //}
+            if (first && second)
+            {
+                label3.Text = ((double)num1 - (double)num2).ToString();
+            }
+            else
+            {
+                label3.Text = "ensira dois valores válidos";
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Text = "*";
+            //if (int.TryParse(textBox1.Text, out int num1))
+            //{
+            //    first = true;
+            //}
+            //else
+            //{
+            //    first = false;
+            //    label3.Text = "somente números, por favor";
+            //}
+            //if (int.TryParse(textBox2.Text, out int num2))
+            //{
+            //    second = true;
+            //}
+            //else
+            //{
+            //    second = false;
+            //    label3.Text = "somente números, por favor";
+            //}
+            if (first && second)
+            {
+                label3.Text = ((double)num1 * (double)num2).ToString();
+            }
+            else
+            {
+                label3.Text = "ensira dois valores válidos";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label2.Text = "/";
+            //if (int.TryParse(textBox1.Text, out int num1))
+            //{
+            //    first = true;
+            //}
+            //else
+            //{
+            //    first = false;
+            //    label3.Text = "somente números, por favor";
+            //}
+            //if (int.TryParse(textBox2.Text, out int num2))
+            //{
+            //    second = true;
+            //}
+            //else
+            //{
+            //    second = false;
+            //    label3.Text = "somente números, por favor";
+            //}
+            if (first && second)
+            {
+                label3.Text = ((double)num1 / (double)num2).ToString();
+            }
+            else
+            {
+                label3.Text = "ensira dois valores válidos";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double num2))
+            {
+                second = true;
+            }
+            else
+            {
+                second = false;
+                label3.Text = "somente números, por favor";
+            }
+            ;
         }
     }
 }
